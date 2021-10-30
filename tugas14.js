4ca4b289365f5036339ec258c7ed671515722b57
mazajiputra <mazajiputra@gmail.com>
2021-10-31 05:17:57 +0700

    Kumpul tugas

---
+++ tugas14.js
@@ -0,0 +1,5 @@
+var x =37/10;
+console.log("Hasil pembagian :",x)
+console.log("Hasil round pembagian :",Math.round(x))
+console.log("Hasil ceil pembagian :",Math.ceil(x))
+console.log("Hasil floor pembagian :",Math.floor(x))
